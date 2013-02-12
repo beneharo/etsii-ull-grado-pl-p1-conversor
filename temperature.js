35540cd469708044f8893a6d09f036b0ca0cb69b
Beneharo González <beng134@gmail.com>
2013-02-12 16:30:10 +0000

    Implementación del código para calcular la conversión ºC <-> F.

---
+++ temperature.js
@@ -0,0 +1,25 @@
+function calculate() {
+    var result;
+    var original = document.getElementById("original");
+    var converted = document.getElementById("converted");
+    var temp = original.value;
+    var regexp = /-?[0-9]+(\.[0-9]+)?\s?(C|c|F|f)/g;
+    
+    var m = temp.match(regexp);
+    
+    if (m) {
+        var num = m.toString().match(/-?[0-9]+(\.[0-9]+)?/gi);
+        var type = m.toString().match(/c|f/i);
+        num = parseFloat(num);
+        if (type == 'c' || type == 'C') {
+            result = (num * 9 / 5) + 32;
+            result = result + "C";
+        } else {
+            result = (num - 32) * 5 / 9;
+            result = result + "F";
+        }
+        converted.innerHTML = result;
+    } else {
+        converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    }
+}
